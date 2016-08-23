142c9104656ceb4dcc1151d839a3f9f973494c41
tpdbs953 <tpdbs953@naver.com>
2016-08-23 16:38:20 +0900

    modal insert

--- app/src/main/java/com/seyun/healthtrainer/Activity/MemberSelectActivity.java
+++ app/src/main/java/com/seyun/healthtrainer/Activity/MemberSelectActivity.java
@@ -1,11 +1,16 @@
 package com.seyun.healthtrainer.Activity;
 
 import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,8 +26,8 @@ public class MemberSelectActivity extends AppCompatActivity implements View.OnCl
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private Button insert, save, start;
-
+    private Button insert, save, start, load;
+    private CoordinatorLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +42,24 @@ public class MemberSelectActivity extends AppCompatActivity implements View.OnCl
     }
 
     private void layoutBind() {
+        // btn bind
         insert = (Button) findViewById(R.id.select_member_btn_insert);
         save = (Button) findViewById(R.id.select_member_btn_save);
         start = (Button) findViewById(R.id.select_member_btn_start);
-
+        load = (Button) findViewById(R.id.select_member_btn_load);
         insert.setOnClickListener(this);
         save.setOnClickListener(this);
         start.setOnClickListener(this);
+        load.setOnClickListener(this);
+
+        // layout bind
+        layout = (CoordinatorLayout) findViewById(R.id.select_member_layout);
+
+        //toolbar bind
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     private void recyclerViewBind() {
@@ -57,6 +73,8 @@ public class MemberSelectActivity extends AppCompatActivity implements View.OnCl
         // specify an adapter (see also next example)
         mAdapter = new SelectMemeberAdapter(getUserInformation(), this);
         mRecyclerView.setAdapter(mAdapter);
+
+
     }
 
     private List<SelectMemberModel> getUserInformation() {
@@ -66,7 +84,6 @@ public class MemberSelectActivity extends AppCompatActivity implements View.OnCl
         userList.add(new SelectMemberModel(true, "김대준"));
         userList.add(new SelectMemberModel(true, "박주찬"));
         userList.add(new SelectMemberModel(true, "이재일"));
-
         return userList;
     }
 
@@ -82,21 +99,54 @@ public class MemberSelectActivity extends AppCompatActivity implements View.OnCl
                 break;
             case R.id.select_member_btn_save:
                 new MaterialDialog.Builder(this)
-                        .title("즐겨찾기")
-                        .content("입력하기")
-                        .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
-                        .input("ㅅㄷㄴㅅ", "123123ㄴ", new MaterialDialog.InputCallback() {
+                        .title("즐겨찾기 추가")
+                        .content("즐겨찾기 이름을 입력해주세요. " +
+                                "(현재인원을 동시에불러오실수있습니다.)")
+                        .negativeText(android.R.string.cancel)
+                        .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_NULL)
+                        .input(null, null, new MaterialDialog.InputCallback() {
                             @Override
                             public void onInput(MaterialDialog dialog, CharSequence input) {
-                                // Do something
+                                Snackbar.make(layout, input+"님 추가완료.", Snackbar.LENGTH_LONG).show();
                             }
-                        }).show();
+                        })
+                        .show();
+                break;
+            case R.id.select_member_btn_load:
+                new MaterialDialog.Builder(this)
+                        .title("즐겨찾기 목록")
+                        .content("불러오실 즐겨찾기 항목을 체크해주세요")
+                        .negativeText(android.R.string.cancel)
+                        .positiveText(android.R.string.ok)
+                        .adapter(mAdapter, null)
+                        .show();
                 break;
             case R.id.select_member_btn_insert:
+                new MaterialDialog.Builder(this)
+                        .title("인원추가")
+                        .content("이름을 입력해주세요")
+                        .negativeText(android.R.string.cancel)
+                        .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_NULL)
+                        .input(null, null, new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
+                                Snackbar snackbar = Snackbar
+                                        .make(layout, input+"님 추가완료.", Snackbar.LENGTH_LONG);
+
+                                snackbar.show();
+                            }
+                        }).show();
                 break;
             default:
                 break;
         }
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
